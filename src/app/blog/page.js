0980a88805b41cdf0f586c2047fa803322e997d0
ekalhor <elmira.kalhor@gmail.com>
2025-04-18 14:10:25 -0700

    pages added for portfolio and blog

---
+++ src/app/blog/page.js
@@ -0,0 +1,16 @@
+export default function BlogPage() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f5eaff',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1 style={{ fontSize: '2rem', color: '#4b0082' }}>
+        Blog page coming soon...
+      </h1>
+    </main>
+  );
+}
